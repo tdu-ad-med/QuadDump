45180a7fc8d4e3111011f111f320b246f4ead220
wakewakame <0214.taiki@gmail.com>
2021-07-16 06:42:11 +0900

    一旦コミット

--- QuadDump/Recorder/QuadRecorder.swift
+++ QuadDump/Recorder/QuadRecorder.swift
@@ -1,149 +1,157 @@
 import Foundation
+import GRDB
 
 class QuadRecorder {
-    // setterのみをプライベートにする
-    public private(set) var status: Status = .disable
+	// setterのみをプライベートにする
+	public private(set) var status: Status = .disable
 
-    private let camRecorder = CamRecorder()
-    private let imuRecorder = IMURecorder()
-    private let gpsRecorder = GPSRecorder()
+	private var dbQueue: DatabaseQueue?
 
-    deinit {
-        let _ = disable()
-    }
+	private let camRecorder = CamRecorder()
+	private let imuRecorder = IMURecorder()
+	private let gpsRecorder = GPSRecorder()
 
-    func preview(camPreview: ((CamRecorder.CamPreview) -> ())?) {
-        camRecorder.preview(camPreview)
-    }
-    func preview(imuPreview: ((IMURecorder.IMUPreview) -> ())?) {
-        imuRecorder.preview(imuPreview)
-    }
-    func preview(gpsPreview: ((GPSRecorder.GPSPreview) -> ())?) {
-        gpsRecorder.preview(gpsPreview)
-    }
+	deinit {
+		let _ = disable()
+	}
 
-    func timestamp(cam: ((TimeInterval, Double) -> ())?) {
-        camRecorder.timestamp(cam)
-    }
-    func timestamp(imu: ((TimeInterval, Double) -> ())?) {
-        imuRecorder.timestamp(imu)
-    }
-    func timestamp(gps: ((TimeInterval, Double) -> ())?) {
-        gpsRecorder.timestamp(gps)
-    }
+	func preview(camPreview: ((CamRecorder.CamPreview) -> ())?) {
+		camRecorder.preview(camPreview)
+	}
+	func preview(imuPreview: ((IMURecorder.IMUPreview) -> ())?) {
+		imuRecorder.preview(imuPreview)
+	}
+	func preview(gpsPreview: ((GPSRecorder.GPSPreview) -> ())?) {
+		gpsRecorder.preview(gpsPreview)
+	}
 
-    // センサーへのアクセスを開始
-    func enable() -> SimpleResult {
-        if case let .failure(e) = camRecorder.enable() { return Err(e.description) }
-        if case let .failure(e) = imuRecorder.enable() { return Err(e.description) }
-        if case let .failure(e) = gpsRecorder.enable() { return Err(e.description) }
-        status = .idol
-        return Ok()
-    }
+	func timestamp(cam: ((TimeInterval, Double) -> ())?) {
+		camRecorder.timestamp(cam)
+	}
+	func timestamp(imu: ((TimeInterval, Double) -> ())?) {
+		imuRecorder.timestamp(imu)
+	}
+	func timestamp(gps: ((TimeInterval, Double) -> ())?) {
+		gpsRecorder.timestamp(gps)
+	}
 
-    // センサーへのアクセスを終了
-    func disable() -> SimpleResult {
-        let _ = stop()
-        if case let .failure(e) = camRecorder.disable() { return Err(e.description) }
-        if case let .failure(e) = imuRecorder.disable() { return Err(e.description) }
-        if case let .failure(e) = gpsRecorder.disable() { return Err(e.description) }
-        status = .disable
-        return Ok()
-    }
+	// センサーへのアクセスを開始
+	func enable() -> SimpleResult {
+		if case let .failure(e) = camRecorder.enable() { return Err(e.description) }
+		if case let .failure(e) = imuRecorder.enable() { return Err(e.description) }
+		if case let .failure(e) = gpsRecorder.enable() { return Err(e.description) }
+		status = .idol
+		return Ok()
+	}
 
-    // 録画開始
-    func start(error: ((String) -> ())? = nil) {
-        if case .disable   = status { error?("センサーへアクセスしていません"); return }
-        if case .recording = status { error?("録画は既に開始しています"); return }
+	// センサーへのアクセスを終了
+	func disable() -> SimpleResult {
+		let _ = stop()
+		if case let .failure(e) = camRecorder.disable() { return Err(e.description) }
+		if case let .failure(e) = imuRecorder.disable() { return Err(e.description) }
+		if case let .failure(e) = gpsRecorder.disable() { return Err(e.description) }
+		status = .disable
+		return Ok()
+	}
 
-        // 録画開始時刻の記録
-        let startDate = Date()
-        let startTime = ProcessInfo.processInfo.systemUptime
+	// 録画開始
+	func start(error: ((String) -> ())? = nil) {
+		if case .disable   = status { error?("センサーへアクセスしていません"); return }
+		if case .recording = status { error?("録画は既に開始しています"); return }
 
-        // 保存先のフォルダを作成
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd_HH-mm-ss"
-        let outputDirName = formatter.string(from: startDate)
-        guard let outputDir = URL.docs?.createDir(name: outputDirName) else {
-            error?("保存先フォルダの作成に失敗しました")
-            return
-        }
+		// 録画開始時刻の記録
+		let startDate = Date()
+		let startTime = ProcessInfo.processInfo.systemUptime
 
-        // 録画に関する情報を設定
-        let info = Info(
-            startDate: startDate,
-            startTime: startTime,
-            outputDir: outputDir
-        )
+		// 保存先のフォルダを作成
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd_HH-mm-ss_SSS"
+		let outputDirName = formatter.string(from: startDate)
+		guard let outputDir = URL.docs?.createDir(name: outputDirName) else {
+			error?("保存先フォルダの作成に失敗しました")
+			return
+		}
 
-        // 映像の情報を記述したテキストファイルを保存
-        guard
-            let colorCamInfo = camRecorder.colorCamInfo,
-            let videoInfo = try? JSONSerialization.data(withJSONObject: [
-                "camera.mp4": [
-                    "width": colorCamInfo.0,
-                    "height": colorCamInfo.1,
-                    "format": "hevc"
-                ],
-                "depth": [
-                    "width": camRecorder.depthCamInfo?.0 ?? 0,
-                    "height": camRecorder.depthCamInfo?.1 ?? 0,
-                    "format": "{uint64(zlibSize),zlib(float[height][width])}[frame]"
-                ],
-                "confidence": [
-                    "width": camRecorder.confidenceCamInfo?.0 ?? 0,
-                    "height": camRecorder.confidenceCamInfo?.1 ?? 0,
-                    "format": "{uint64(zlibSize),zlib(uint8[height][width])}[frame]"
-                ],
-                "cameraFrameInfo": [
-                    "format": "raw,{uint64(frameNumber),double(timestampe),uint8(isColorFrameExist),uint8(isDepthFrameExist),uint8(isConfidenceFrameExist),uint64(depthOffset),uint64(confidenceOffset),float3x3(intrinsics),float4x4(projectionMatrix),float4x4(viewMatrix)}[frame]"
-                ],
-                "imu": [
-                    "format": "raw,{double(timestampe),double3(gravity),double3(userAccleration),double3(attitude)}[frame]"
-                ],
-                "gps": [
-                    "format": "raw,{double(timestampe),double(latitude),double(longitude),double(altitude),double(horizontalAccuracy),double(verticalAccuracy)}[frame]"
-                ]
-            ], options: []),
-            FileManager.default.createFile(
-                atPath: outputDir.appendingPathComponent("info.json").path,
-                contents: videoInfo,
-                attributes: nil
-            )
-        else { error?("ファイルの書き込みに失敗しました"); return }
+		// 保存先のデータベースを作成
+		dbQueue = try? DatabaseQueue(path: outputDir.appendingPathComponent("db.sqlite3").path)
+		guard let dbQueue = dbQueue else {
+			error?("データベースの作成に失敗しました")
+			return
+		}
 
-        // 各センサーの録画開始
-        camRecorder.start(outputDir, info.startTime) { e in error?(e) }
-        imuRecorder.start(outputDir, info.startTime) { e in error?(e) }
-        gpsRecorder.start(outputDir, info.startTime) { e in error?(e) }
+		// 映像の情報を記録
+		guard let _ = ( try? dbQueue.write { db in
+			struct Description: Codable, FetchableRecord, PersistableRecord {
+				var date: Date
+				var color_width : Int
+				var color_height: Int
+				var depth_width : Int?
+				var depth_height: Int?
+				var confidence_width : Int?
+				var confidence_height: Int?
+			}
+			try db.create(table: "description") { t in
+				t.column("date", .datetime).notNull()
+				t.column("color_width" , .integer).notNull()
+				t.column("color_height", .integer).notNull()
+				t.column("depth_width" , .integer)
+				t.column("depth_height", .integer)
+				t.column("confidence_width" , .integer)
+				t.column("confidence_height", .integer)
+			}
+			try Description(
+				date: startDate,
+				color_width : camRecorder.colorCamInfo!.0,
+				color_height: camRecorder.colorCamInfo!.1,
+				depth_width : camRecorder.depthCamInfo?.0,
+				depth_height: camRecorder.depthCamInfo?.1,
+				confidence_width : camRecorder.confidenceCamInfo?.0,
+				confidence_height: camRecorder.confidenceCamInfo?.1
+			).insert(db)
+		} )
+		else { error?("データベースの書き込みに失敗しました"); return }
 
-        status = .recording(info)
-    }
+		// 録画に関する情報を設定
+		let info = Info(
+			startDate: startDate,
+			startTime: startTime,
+			outputDir: outputDir,
+            dbQueue  : dbQueue
+		)
 
-    // 録画終了
-    func stop(error: ((String) -> ())? = nil) {
-        if case .disable = status { error?("センサーへアクセスしていません") }
-        if case .idol    = status { error?("録画は既に終了しています") }
+		// 各センサーの録画開始
+		camRecorder.start(outputDir, info.startTime) { e in error?(e) }
+		imuRecorder.start(outputDir, info.startTime) { e in error?(e) }
+		gpsRecorder.start(outputDir, info.startTime) { e in error?(e) }
 
-        // 各センサーの録画終了
-        camRecorder.stop { e in error?(e) }
-        imuRecorder.stop { e in error?(e) }
-        gpsRecorder.stop { e in error?(e) }
+		status = .recording(info)
+	}
 
-        status = .idol
-    }
+	// 録画終了
+	func stop(error: ((String) -> ())? = nil) {
+		if case .disable = status { error?("センサーへアクセスしていません") }
+		if case .idol	= status { error?("録画は既に終了しています") }
 
-    // 録画に関する情報
-    struct Info {
-        let startDate: Date          // 録画開始時刻(日時)
-        let startTime: TimeInterval  // 録画開始時刻(デバイスを起動してからのTimeInterval)
-        let outputDir: URL           // 保存先ディレクトリ
-    }
+		// 各センサーの録画終了
+		camRecorder.stop { e in error?(e) }
+		imuRecorder.stop { e in error?(e) }
+		gpsRecorder.stop { e in error?(e) }
 
-    // 録画状態
-    enum Status {
-        case disable          // センサーにアクセスしていない状態
-        case idol             // 録画していない状態
-        case recording(Info)  // 録画中
-    }
+		status = .idol
+	}
+
+	// 録画に関する情報
+	struct Info {
+		let startDate: Date		      // 録画開始時刻(日時)
+		let startTime: TimeInterval   // 録画開始時刻(デバイスを起動してからのTimeInterval)
+		let outputDir: URL		      // 保存先ディレクトリ
+        let dbQueue  : DatabaseQueue  // 保存先データベース
+	}
+
+	// 録画状態
+	enum Status {
+		case disable		  // センサーにアクセスしていない状態
+		case idol			 // 録画していない状態
+		case recording(Info)  // 録画中
+	}
 }
